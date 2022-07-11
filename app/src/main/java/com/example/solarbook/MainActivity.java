7b3b54c05d79d88846c67a5907487bd051b5e620
Nguyen Tuan Son <tieuthantai3112@gmail.com>
2022-07-11 11:44:26 +0700

    Tạo Navigation Bottom Bar, chuyển đổi giữa các Fragment

--- app/src/main/java/com/example/solarbook/MainActivity.java
+++ app/src/main/java/com/example/solarbook/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.solarbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.solarbook.adapters.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mNavigationView;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavigationView = findViewById(R.id.bottomNav);
+        mViewPager = findViewById(R.id.viewPager);
+
+        setUpViewPager();
+        mNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menuHome:
+                        mViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.menuCategory:
+                        mViewPager.setCurrentItem(1);
+                        break;
+                    case R.id.menuNews:
+                        mViewPager.setCurrentItem(2);
+                        break;
+                    case R.id.menuAccount:
+                        mViewPager.setCurrentItem(3);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    //hàm thiết lập ViewPager
+    private void setUpViewPager(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        mNavigationView.getMenu().findItem(R.id.menuHome).setChecked(true);
+                        break;
+                    case 1:
+                        mNavigationView.getMenu().findItem(R.id.menuCategory).setChecked(true);
+                        break;
+                    case 2:
+                        mNavigationView.getMenu().findItem(R.id.menuNews).setChecked(true);
+                        break;
+                    case 3:
+                        mNavigationView.getMenu().findItem(R.id.menuAccount).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/solarbook/adapters/ViewPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.solarbook.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.solarbook.fragments.AccountFragment;
+import com.example.solarbook.fragments.CategoryFragment;
+import com.example.solarbook.fragments.HomeFragment;
+import com.example.solarbook.fragments.NewsFragment;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new CategoryFragment();
+            case 2:
+                return new NewsFragment();
+            case 3:
+                return new AccountFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
